ae17ae06c83df59472ca0fcc6a8efed8ea4035a0
BlueDragonC45 <55166235+BlueDragonC45@users.noreply.github.com>
2019-10-26 17:32:18 -0400

    Added js for seperate downloads page

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-window.alert('Welcome to AIM.Studio We hope you are well.');
+//window.alert('Welcome to AIM.Studio We hope you are well.');
+
+function popup() {
+	var element = document.getElementById("popup");
+	element.classList.remove("hidden");
+}
+
+function showselection() {
+	var element = document.getElementById("dpage2");
+	var element2 = document.getElementById("popup");
+	element.classList.remove("hidden");
+	element2.classList.add("hidden");
+}
+
+function showthanks() {
+	var element = document.getElementById("dpage3");
+	var element2 = document.getElementById("dpage2");
+	var element3 = document.getElementById("dpage");
+	element.classList.remove("hidden");
+	element2.classList.add("hidden");
+	element3.classList.add("hidden");
+}
