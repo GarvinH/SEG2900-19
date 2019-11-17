c2275e56b56872773bb465628bbbd0d8b9b1cf79
BlueDragonC45 <55166235+BlueDragonC45@users.noreply.github.com>
2019-11-17 14:31:30 -0500

    Download page finishing

--- main.js
+++ main.js
@@ -14,11 +14,15 @@ function showbuttons() {
 	document.getElementById("dheader").textContent = "Select your version:";
 	document.getElementById("dpara").textContent = "Here you have two options, Lite or Pro. Lite is the free version, for those looking for simple songs. Pro is for those looking to get the most out of the AIM.Studio experience, with many more options right at your finger tips! ";
 	document.getElementById("dpara2").textContent = " "; 
+	document.getElementById("buttonposlite").classList.remove("hidden");
+	document.getElementById("buttonpospro").classList.remove("hidden");
 }
 
 function showthanksmessage() {
 	document.getElementById("dheader").textContent = "Thank you for your support, good luck in your producing!";
 	document.getElementById("dpara").textContent = "Your download should start momentarily";
+	document.getElementById("buttonposlite").classList.add("hidden");
+	document.getElementById("buttonpospro").classList.add("hidden");
 }
 
 
